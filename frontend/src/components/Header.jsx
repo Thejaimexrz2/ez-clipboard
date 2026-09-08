91b99758f8552ae9703caf0becaadafdb8af1a7c
Jaime Ferrate <jaferrate-spec>
2026-09-08 13:38:27 -0300

    Commit inicial, base de frontend agregada. Se añade Header.

---
+++ frontend/src/components/Header.jsx
@@ -0,0 +1,11 @@
+import '../css/header.css'
+
+export default function Header() {
+    return (
+        <header className="header">
+            <div className="app-name-container">
+                <h1 className="app-name">Ez Clipboard</h1>
+            </div>
+        </header>
+    )
+}
