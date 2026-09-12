2f2a1b6ce33421ae963eb83bfe23348331373e80
jaferrate-spec <ja.ferrate@duocuc.cl>
2026-09-12 16:26:34 -0300

    Se agrega API Flask, metodos basicos y correcion a interfaz.

--- frontend/src/components/SendForm.jsx
+++ frontend/src/components/SendForm.jsx
@@ -1,15 +1,26 @@
-import '../css/sendform.css'
+import "../css/sendform.css";
 
 export default function SendForm() {
+    function sendForm() {}
+
     return (
         <form className="sendForm-container">
             <label htmlFor="text">Contenido: </label>
-            <textarea rows={10} cols={10} id="text" placeholder="Tu texto acá..."></textarea>
-            
+            <textarea
+                rows={10}
+                cols={10}
+                id="text"
+                placeholder="Tu texto acá..."
+            ></textarea>
+
             <div className="buttonContainer">
-                <button formAction={SendForm} id="upload">Subir Texto</button>
-                <p className="code-preview">Tu codigo para compartir: Nulo</p>
+                <button formAction={SendForm} id="upload">
+                    Subir Texto
+                </button>
+                <p className="code-preview" hidden>
+                    Tu codigo para compartir: Nulo
+                </p>
             </div>
         </form>
-    )
+    );
 }
